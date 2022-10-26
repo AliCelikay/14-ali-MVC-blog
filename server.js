aa6139882236472e77541b2226c3a3176d54b665
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-10-25 19:39:40 -0500

    added routes & fixed errors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // 3rd party packages
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
 // Custom Packages
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
